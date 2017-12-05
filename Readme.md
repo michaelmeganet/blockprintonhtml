015f173d6431b60669b12beac9d8b6bb47cbc964
C.T.Chong <cct3000@gmail.com>
2017-12-05 14:27:50 +0800

    5-12-2107 complete

---
+++ Readme.md
@@ -0,0 +1,14 @@
+The code prevent print from html and display an alert to show user they can not print this page
+
+-> cannotprintme.html
+
+The code here do not show any reason for why can not prin
+
+-> cant-print-html2.html
+
+refer to https://www.tjvantoll.com/2012/06/15/detecting-print-requests-with-javascript/
+
+-> detectprint.html
+
+function.js and styles.css are use in cant-print-me.html
+
--- function.js
+++ function.js
@@ -1,25 +1,22 @@
-if ('matchMedia' in window) {
-    // Chrome, Firefox, and IE 10 support mediaMatch listeners
-    window.matchMedia('print').addListener(function(media) {
-        if (media.matches) {
-            beforePrint();
-        } else {
-            // Fires immediately, so wait for the first mouse movement
-            $(document).one('mouseover', afterPrint);
-        }
-    });
-} else {
-    // IE and Firefox fire before/after events
-    $(window).on('beforeprint', beforePrint);
-    $(window).on('afterprint', afterPrint);
-}
+		if (window.matchMedia) {
+                var mediaQueryList = window.matchMedia('print');
+                mediaQueryList.addListener(function(mql) {
+                    if (mql.matches) {
+                        beforePrint();
+                    } else {
+                        afterPrint();
+                    }
+                });
+            }
 
-function beforePrint() {
-    $("#AllContent").hide();
-    $(".PrintMessage").show();
-}
+		function beforePrint() {
+		    $("#AllContent").hide();
+		    $(".PrintMessage").show();
+		}
 
-function afterPrint() {
-    $(".PrintMessage").hide();
-    $("#AllContent").show();
-}
+		function afterPrint() {
+		    $(".PrintMessage").hide();
+		    $("#AllContent").show();
+		}
+		window.onbeforeprint = beforePrint;
+        window.onafterprint = afterPrint;
